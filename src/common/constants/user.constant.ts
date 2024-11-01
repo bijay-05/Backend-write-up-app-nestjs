66c1ba95b68188227d20957aca965118858964b2
bijay-05 <hawkeyesniper69@gmail.com>
2024-11-01 17:10:34 +0545

    feat: add user msg consts

---
+++ src/common/constants/user.constant.ts
@@ -0,0 +1,7 @@
+export const USER_MESSAGE_CONSTANT = {
+    SUCCESS_MESSAGE: {
+        USER_FETCH_SUCCESS: "User fetched successfully",
+        USER_CREATE_SUCCESS: "User created successfully",
+        USER_UPDATE_SUCCESS: "User updated successfully"
+    }
+}
