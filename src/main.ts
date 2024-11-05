31a0835e2acbaeb8516261184b5dc17c54c4f5b5
bijay-05 <hawkeyesniper69@gmail.com>
2024-11-05 15:03:33 +0545

    feat: add logging to main app

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppExceptionFilter, PrismaExceptionFilter } from 'src/common/filters';
 import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { logger: ["log", "error"]});
 
   const configService = app.get(ConfigService)
   const PORT = configService.get<number>("PORT")
