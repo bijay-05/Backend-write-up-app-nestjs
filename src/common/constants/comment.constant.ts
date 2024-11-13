65b27ffa190c420a67219750d27e8c7888dde185
bijay-05 <hawkeyesniper69@gmail.com>
2024-11-13 16:08:39 +0545

    feat: add msg consts for comment resource

---
+++ src/common/constants/comment.constant.ts
@@ -0,0 +1,8 @@
+export const COMMENT_MESSAGE_CONST = {
+    SUCCESS_MESSAGE: {
+        COMMENT_FETCH_SUCCESS: "Comments fetched successfully",
+        COMMENT_CREATE_SUCCESS: "Comment created successfully",
+        COMMENT_UPDATE_SUCCESS: "Comment updated successfully",
+        COMMENT_DELETE_SUCCESS: "Comment deleted successfully"
+    }
+}
