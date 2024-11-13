126deb6519a45975bed9f19bea5085b26c2cce28
bijay-05 <hawkeyesniper69@gmail.com>
2024-11-13 14:29:49 +0545

    refactor name

---
+++ src/common/constants/blog.constant.ts
@@ -0,0 +1,11 @@
+export const BLOG_MESSAGE_CONSTANT = {
+    SUCCESS_MESSAGE: {
+        BLOG_FETCH_SUCCESS: "Blog fetched successfully",
+        BLOG_CREATE_SUCCESS: "Blog created successfully",
+        BLOG_UPDATE_SUCCESS: "Blog updated successfully",
+        BLOG_DELETE_SUCCESS: "Blog deleted successfully"
+    },
+    ERROR_MESSAGE: {
+        BLOG_NOT_FOUND: "Blog not found"
+    }
+}
